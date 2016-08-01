b34f64697e77e7b8912fe761d1e2e5be7ae7afef
qianfeng sdl <qianfeng_sdl@qianfengsakura-mini.local>
2016-08-01 14:50:16 +0800

    Initial Commit

---
+++ Demo02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo02
+//
+//  Created by qianfeng_sdl on 16/8/1.
+//  Copyright © 2016年 songdongliang_sdl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo02/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo02
+//
+//  Created by qianfeng_sdl on 16/8/1.
+//  Copyright © 2016年 songdongliang_sdl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
