36d01740d58f2f6aec654aff1e8e4b8f0116f9d7
qianfeng_sdl <qianfeng_sdl@qianfengsakura-mini.local>
2016-08-01 21:47:03 +0800

    新的model

---
+++ Demo02/SongDongLiangModel.h
@@ -0,0 +1,13 @@
+//
+//  SongDongLiangModel.h
+//  Demo02
+//
+//  Created by qianfeng_sdl on 16/8/1.
+//  Copyright © 2016年 songdongliang_sdl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SongDongLiangModel : JSONModel
+
+@end
---
+++ Demo02/SongDongLiangModel.m
@@ -0,0 +1,13 @@
+//
+//  SongDongLiangModel.m
+//  Demo02
+//
+//  Created by qianfeng_sdl on 16/8/1.
+//  Copyright © 2016年 songdongliang_sdl. All rights reserved.
+//
+
+#import "SongDongLiangModel.h"
+
+@implementation SongDongLiangModel
+
+@end
